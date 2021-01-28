28dec7c7e6b8a5eefdb5b6d8d3dac0a813192373
Ryan Jeon <ryandhjeon@gmail.com>
2021-01-28 11:45:45 -0500

    Minor change added

--- part1.sh
+++ part1.sh
@@ -13,14 +13,14 @@ hdfs dfs -ls /activity1/data/
 # 4. Move the file /activity1/data/RandomText.txt to /activity1/data/NotSoRandomText.txt
 hdfs dfs -mv /activity1/data/RandomText.txt /activity1/data/NotSoRandomText.txt
 
-# 5. Append the file RandomText.txt to the end of the file: /activity1/data/RandomText.txt
+# 5. Append the local file RandomText.txt to the end of the HDFS file: /activity1/data/NotSoRandomText.txt
 hdfs dfs -appendToFile /Workspace/cs6500_sp2021_r02_jeon/RandomText.txt /activity1/data/NotSoRandomText.txt
 
 # 6. List the disk space used by the directory /activity1/data/
 hdfs dfs -du -s -h /activity1/data/
 
-# 7. Put the file MoreRandomText.txt into HDFS as the path: /activity1/data/RandomText.txt
-hdfs dfs -put /Workspace/cs6500_sp2021_r02_jeon/MoreRandomText.txt /activity1/data/
+# 7. Put the local file MoreRandomText.txt into HDFS as the path: /activity1/data/MoreRandomText.txt
+hdfs dfs -put /Workspace/cs6500_sp2021_r02_jeon/MoreRandomText.txt /activity1/data/MoreRandomText.txt
 
 # 8. Recursively list the contents of the directory /activity1/
 hdfs dfs -ls -R /activity1/
